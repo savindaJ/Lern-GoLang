61ebbd47d88e05c535416671981714fb21acdb3b
savindaJ <thantrige32@gmail.com>
2025-12-28 21:51:11 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age int
+	Email string
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("Name: %s, Age: %d, Email: %s", p.Name, p.Age, p.Email)
+}
+
+func sayHello(name string) {
+	fmt.Println("Hello, " + name)
+}
+
+func runAsync() {
+    ch := make(chan string)
+    go func() {
+		sayHello("Savinda")
+		person := Person{Name: "Savinda", Age: 20, Email: "savinda@gmail.com"}
+		fmt.Println(person)
+        ch <- "done"
+    }()
+    fmt.Println(<-ch)
+}
+
+
+
+func main() {
+	
+	runAsync()
+}
+
